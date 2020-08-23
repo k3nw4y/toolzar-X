cccf5699e5bbbd041f0ed09fad6f307f15e0cf9a
Elliot Alderson <46229190+k3nw4y@users.noreply.github.com>
2020-08-23 22:51:01 +0530

    ok lol

---
+++ build/precache-manifest.8522ff6cbebb9c2679d3b4b548972930.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433553f31b79f2e4f97a964d6b3b2d81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8a2eca4ebe2646630bd",
+    "url": "/static/css/main.3b94e852.chunk.css"
+  },
+  {
+    "revision": "d879613085e5a459541e",
+    "url": "/static/js/2.678978d5.chunk.js"
+  },
+  {
+    "revision": "d8a2eca4ebe2646630bd",
+    "url": "/static/js/main.18168f11.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
